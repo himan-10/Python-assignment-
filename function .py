86cd6cfd923ff3bfd4f32c81ab6c0feaa610a600
himan-10 <134701789+himan-10@users.noreply.github.com>
2023-05-30 03:45:35 +0530

    Create function .py

---
+++ function .py
@@ -0,0 +1,18 @@
+def calculateGmean(a, b):
+    mean=(a-b)
+    print(mean)
+def isGreater (a,b):
+        if(a>b):
+            print("fist number is the greater")
+        else:
+            print("second number ius greater or equal")
+def islesser (a,b):
+    pass
+a=9
+b=2
+isGreater(a, b)
+calculateGmean(a,b)
+c=1000
+d=121
+isGreater(a,b)
+calculateGmean(c,d)
